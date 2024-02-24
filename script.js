3dfce8cff40dff585340e5770e5242c7049b550a
miwanoff <marina.iva.noffa@gmail.com>
2024-02-24 13:27:15 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let promise = new Promise(function (resolve, reject) {
+  setTimeout(() => resolve("Success!"), 1000); // задача выполнена успешно (отработает через 1 секунду)
+});
+
+promise.then(
+  function (result) {
+    /* обработка успешного выполнения */
+    alert(result);
+  },
+  function (error) {
+    /* обработка ошибки */
+    alert(error);
+  }
+);
+
+promise.catch(() => alert("Error"));
+promise.finally(() => alert("Finally"))
